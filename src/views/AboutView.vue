952ad90a7b8e833055df5b25e1fe1768dc76e85e
miracle miracly <mht12345mht12345@gmail.com>
2025-01-06 15:10:22 +0330

    background has been set

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -7,8 +7,8 @@ import { RouterLink } from 'vue-router';
      <h1 class="text-6xl font-bold mb-4">
           About the page
      </h1>
-     <p class="text-xl mb-5">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt magnam ab nesciunt error cum laboriosam aut molestias, alias voluptatibus magni? Dolores et aliquid accusantium numquam ducimus facere, ullam totam. Veniam.
+     <p class="text-xl  bg-slate-400 mb-5">
+         sit amet consectetur adipisicing elit. Incidunt magnam ab nesciunt error cum laboriosam aut molestias, alias voluptatibus magni? Dolores et aliquid accusantium numquam ducimus facere, ullam totam. Veniam.
      </p>
      <RouterLink to="/" class="text-white bg-green-700 hover:bg-green-900 rounded-md px-3 py-2 mt-4">Go Back</RouterLink>
 </section>
